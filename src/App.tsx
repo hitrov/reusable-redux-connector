d3170c6397c86bd3a60aa04344b84585f231a1fe
Alexander Hitrov <github.com@hitrov.com>
2020-08-22 11:41:11 +0300

    getConnectedComponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,8 @@ import React from 'react';
 import './App.css';
 import { Content } from "./Content";
 import { ConnectedProps, connect } from "react-redux";
-import { SetLanguage } from "./actions";
 import { RootState } from "./reducers";
 import { setLanguage } from './actions';
-
-// type StateProps = Pick<IProps, | 'language'>;
-// type DispatchProps = Pick<IProps, | 'setLanguage'>;
-// type OwnProps = Omit<
-//   IProps,
-//   keyof StateProps | keyof DispatchProps
-// >;
 
 const mapStateToProps = (state: RootState) => ({
   language: state.language,
@@ -21,22 +13,15 @@ const mapDispatch = {
   setLanguage,
 };
 
-// interface StateProps {
-//   language: string;
-// }
-
-// interface DispatchProps {
-//   setLanguage(language: string): SetLanguage;
-// }
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatch;
-
-// const connector = connect<StateProps, DispatchProps, {}, RootState>(mapStateToProps, mapDispatch);
 const connector = connect(mapStateToProps, mapDispatch);
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
+function getConnectedComponent(Component: React.ComponentType) {
+  return connector(Component);
+}
+
+// const ConnectedContent = getConnectedComponent(Content);
 const ConnectedContent = connector(Content);
 
 function App() {
